76a869f77efe772172d39e679f9dd0ae96307a51
qinghuan <gaowei@yunify.com>
2020-02-17 17:53:09 +0800

    dddd

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,8 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: "module"
   },
-  extends: ["eslint:recommended"]
+  extends: ["eslint:recommended"],
+  rules: {
+    'no-console': 'error'
+  }
 };
